d24d5490f1c4e0e8cadcbfb5d6f1c4d096e6fc57
Ryan Guide <ryan.guide@comcast.net>
2020-10-01 15:09:10 -0400

    starting to adapt to normal sudoku size

---
+++ full_sudoku.py
@@ -0,0 +1,36 @@
+test_puzzle_rows = [[5,3,0,0,7,0,0,0,0],
+                    [6,0,0,1,9,5,0,0,0],
+                    [0,9,8,0,0,0,0,6,0],
+                    [8,0,0,0,6,0,0,0,3],
+                    [4,0,0,8,0,3,0,0,1],
+                    [7,0,0,0,2,0,0,0,6],
+                    [0,6,0,0,0,0,2,8,0],
+                    [0,0,0,4,1,9,0,0,5],
+                    [0,0,0,0,8,0,0,7,9]]
+
+test_solution = [[5,3,4,6,7,8,9,1,2],
+                 [6,7,2,1,9,5,3,4,8],
+                 [1,9,8,3,4,2,5,6,7],
+                 [8,5,9,7,6,1,4,2,3],
+                 [4,2,6,8,5,3,7,9,1],
+                 [7,1,3,9,2,4,8,5,6],
+                 [9,6,1,5,3,7,2,8,4],
+                 [2,8,7,4,1,9,6,3,5],
+                 [3,4,5,2,8,6,1,7,9]]
+#Important idea: Rather than have a set number of loops, have it check changes
+#Each time it changes, increase change count.
+#If change count doesnt increase, end
+
+columns = []
+boxes = []
+def create_columns(rows):
+    "Translates the rows into columns"
+    for n in range(9):
+        column = []
+        for m in range(9):
+            column.insert(m, rows[m][n])
+            continue
+        columns.insert(n, column)
+        continue
+    return columns
+print(create_columns(test_puzzle_rows))

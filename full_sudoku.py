dc38dee33d844aefda696543868c78e4c98f657a
Ryan Guide <ryan.guide@comcast.net>
2020-10-12 14:54:02 -0400

    New Algo starting

--- full_sudoku.py
+++ full_sudoku.py
@@ -114,12 +114,6 @@ def identify_box(n,m):
         elif m in range(6,9):
             return 8
 
-# def box_checker(rows):
-#     for n in range(9):
-#         for m in range(9):
-#             b = identify_box(n,m)
-#             print(b)
-#             continue
 
 possible_numbers = [1,2,3,4,5,6,7,8,9]
 
@@ -170,15 +164,28 @@ def solver(rows):
     return rows
 #print(solver(test_puzzle_rows))
 #print(solver(easy_test))
+#print(solver([[6, 3, 5, 7, 9, 4, 2, 1, 8], [4, 7, 2, 1, 5, 8, 6, 3, 9], [8, 0, 9, 0, 0, 3, 4, 5, 0], [1, 8, 4, 0, 3, 7, 9, 2, 0], [7, 5, 6, 0, 0, 9, 0, 4, 0], [2, 9, 3, 4, 1, 6, 7, 8, 5], [5, 6, 8, 9, 4, 2, 1, 7, 3], [9, 0, 7, 3, 0, 1, 0, 6, 4], [3, 0, 1, 6, 0, 5, 0, 9, 0]]))
+#print(solver([[6, 3, 5, 7, 9, 4, 2, 1, 8], [4, 7, 2, 1, 5, 8, 6, 3, 9], [8, 0, 9, 2, 0, 3, 4, 5, 0], [1, 8, 4, 5, 3, 7, 9, 2, 0], [7, 5, 6, 0, 0, 9, 0, 4, 0], [2, 9, 3, 4, 1, 6, 7, 8, 5], [5, 6, 8, 9, 4, 2, 1, 7, 3], [9, 2, 7, 3, 0, 1, 0, 6, 4], [3, 0, 1, 6, 0, 5, 8, 9, 0]]))
+#print(solver([[6, 3, 5, 7, 9, 4, 2, 1, 8], [4, 7, 2, 1, 5, 8, 6, 3, 9], [8, 1, 9, 2, 0, 3, 4, 5, 0], [1, 8, 4, 5, 3, 7, 9, 2, 6], [7, 5, 6, 8, 0, 9, 0, 4, 0], [2, 9, 3, 4, 1, 6, 7, 8, 5], [5, 6, 8, 9, 4, 2, 1, 7, 3], [9, 2, 7, 3, 8, 1, 0, 6, 4], [3, 4, 1, 6, 0, 5, 8, 9, 0]]))
+#print(solver([[6, 3, 5, 7, 9, 4, 2, 1, 8], [4, 7, 2, 1, 5, 8, 6, 3, 9], [8, 1, 9, 2, 0, 3, 4, 5, 7], [1, 8, 4, 5, 3, 7, 9, 2, 6], [7, 5, 6, 8, 2, 9, 0, 4, 0], [2, 9, 3, 4, 1, 6, 7, 8, 5], [5, 6, 8, 9, 4, 2, 1, 7, 3], [9, 2, 7, 3, 8, 1, 5, 6, 4], [3, 4, 1, 6, 0, 5, 8, 9, 0]]))
+print(solver([[6, 3, 5, 7, 9, 4, 2, 1, 8], [4, 7, 2, 1, 5, 8, 6, 3, 9], [8, 1, 9, 2, 6, 3, 4, 5, 7], [1, 8, 4, 5, 3, 7, 9, 2, 6], [7, 5, 6, 8, 2, 9, 3, 4, 0], [2, 9, 3, 4, 1, 6, 7, 8, 5], [5, 6, 8, 9, 4, 2, 1, 7, 3], [9, 2, 7, 3, 8, 1, 5, 6, 4], [3, 4, 1, 6, 7, 5, 8, 9, 0]]))
 
-
-
-# Search each row for a 0
-# If there is a zero, check what numbers could fit in there by checking the corresponding row, column, and box
-# Push the numbers that fit there into a candidates list
-# If there is only one candidate, enter that and go back to 1 until no zeros remain.
-# If there is more than one option, take the first one then try to fill out the rest of the holes that way
-# If that runs into a contradiction, where there's an empty candidates list, go back to that previous filled in 0 and try the next candidate then go forward again.
-# If that runs into an error, then go back to the 0 before that and change it to the next candidate
-# Continue until no 0's remain
-# Return the finished list of lists
+# In depth Algorithm:
+# Check each row for a 0
+# If there are no 0's, return the rows
+# Generate the column and box data
+# At the first zero, check the corresponding row, column, and box to see which numbers are candidates
+# If there is only one candidate, remove the 0 and put that candidate in, 
+    # then remake the column and box data, clear the candidates and continue to the next 0
+# If there there are at least 2 candidates, input the first candidate, and save a copy of the candidates list with the used number removed.
+    # Store the row, column, box, and copied list in an array called changes
+    # Remake the column and box data, and clear the candidates list
+    # Continue to the next 0
+# If there are NO candidates, grab the last piece of information from the changes list
+    # This will be the last edited number, so go back to that position and remove the number
+    # Replace it with the first number from the copied candidates list, then pop that number from the copied candidates list
+    # Remake column and box data, then proceed from the position that was most recently changed
+        # IF the copied candidates list is EMPTY, remove the entire data entry from the changes list
+        # obtain the information from the last changed data entry repeat the process
+# Continue until all 0's have been correctly replaced
+# Return the rows 
